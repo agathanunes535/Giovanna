96463ebd1c18bfd47cb628c88655f3b21d7652d4
agathanunes535 <giovannaalvesdossantos21@gmail.com>
2024-08-19 15:35:07 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(600, 600);
+  }
+  
+  function draw() {
+    background("pink");
+    
+    stroke("magenta");
+    fill("green");
+  
+  console.log(mouseIsPressend);
+    rect(mouseX, mouseY, 20, 35);
+    
+    
+    
+  }
